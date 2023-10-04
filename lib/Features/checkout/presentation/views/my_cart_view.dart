86b79e80cd9fbdd36607301e07b6cfa5f3619d9a
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-04 12:58:22 +0300

    create cart view

---
+++ lib/Features/checkout/presentation/views/my_cart_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

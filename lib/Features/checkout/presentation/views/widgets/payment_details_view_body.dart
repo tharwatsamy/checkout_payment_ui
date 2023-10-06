06d654467e6a4d84b926fed097f34b68d9ff777b
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-06 11:35:38 +0300

    flutter credit card part one

--- lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:checkout_payment_ui/Features/checkout/presentation/views/widgets/payment_method_item.dart';
 import 'package:checkout_payment_ui/Features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -9,7 +10,39 @@ class PaymentDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [PaymentMethodsListView()],
+      children: [
+        PaymentMethodsListView(),
+        CustomCreditCard(),
+      ],
+    );
+  }
+}
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+
+  bool showBackView = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (value) {},
+        )
+      ],
     );
   }
 }
--- lib/Features/checkout/presentation/views/widgets/payment_method_item.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_method_item.dart
@@ -13,7 +13,7 @@ class PaymentMethodItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: const Duration(milliseconds: 600),
+      duration: const Duration(milliseconds: 300),
       width: 103,
       height: 62,
       decoration: ShapeDecoration(

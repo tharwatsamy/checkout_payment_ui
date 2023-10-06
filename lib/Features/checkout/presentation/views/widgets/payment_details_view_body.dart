1776659454abb8dd6ddcac9ae868c2ca12d836ec
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-06 12:41:57 +0300

    flutter credit card part two

--- lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -9,11 +9,13 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        PaymentMethodsListView(),
-        CustomCreditCard(),
-      ],
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          PaymentMethodsListView(),
+          CustomCreditCard(),
+        ],
+      ),
     );
   }
 }
@@ -29,7 +31,7 @@ class _CustomCreditCardState extends State<CustomCreditCard> {
   String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
 
   bool showBackView = false;
-
+  final GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -40,8 +42,23 @@ class _CustomCreditCardState extends State<CustomCreditCard> {
           cardHolderName: cardHolderName,
           cvvCode: cvvCode,
           showBackView: showBackView,
+          isHolderNameVisible: true,
           onCreditCardWidgetChange: (value) {},
-        )
+        ),
+        CreditCardForm(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            onCreditCardModelChange: (carditCardModel) {
+              cardHolderName = carditCardModel.cardHolderName;
+              expiryDate = carditCardModel.expiryDate;
+              cvvCode = carditCardModel.cvvCode;
+              cardNumber = carditCardModel.cardNumber;
+              showBackView = carditCardModel.isCvvFocused;
+              setState(() {});
+            },
+            formKey: formKey),
       ],
     );
   }

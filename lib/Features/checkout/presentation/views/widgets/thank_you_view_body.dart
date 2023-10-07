5a5c811add8c1fcf317b315184fbc3bd6cff1ab2
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-07 11:09:40 +0300

    create dashed line

--- lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -5,6 +5,69 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+              color: const Color(0xFFD9D9D9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * .2 + 20,
+            left: 20 + 8,
+            right: 20 + 8,
+            child: Row(
+              children: List.generate(
+                30,
+                (index) => Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2),
+                    child: Container(
+                      color: const Color(0xffB8B8B8),
+                      height: 2,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+              left: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: const CircleAvatar(
+                backgroundColor: Colors.white,
+              )),
+          Positioned(
+              right: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: const CircleAvatar(
+                backgroundColor: Colors.white,
+              )),
+          const Positioned(
+            top: -50,
+            left: 0,
+            right: 0,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Color(0xFFD9D9D9),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Color(0xff34A853),
+                child: Icon(
+                  Icons.check,
+                  size: 50,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

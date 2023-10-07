42cd33b71447f08a6929927a05055598368b5e84
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-07 23:06:59 +0300

    create payment item info

--- lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
@@ -9,12 +9,73 @@ class ThankYouCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: ShapeDecoration(
         color: const Color(0xFFD9D9D9),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
       ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50 + 16, left: 22, right: 22),
+        child: Column(
+          children: [
+            const Text(
+              'Thank you!',
+              textAlign: TextAlign.center,
+              style: Styles.style25,
+            ),
+            Text(
+              'Your transaction was successful',
+              textAlign: TextAlign.center,
+              style: Styles.style20,
+            ),
+            const SizedBox(
+              height: 42,
+            ),
+            const PaymentItemInfo(
+              title: 'Date',
+              value: '01/24/2023',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'Time',
+              value: '10:15 AM',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'To',
+              value: 'Sam Louis',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PaymentItemInfo extends StatelessWidget {
+  const PaymentItemInfo({super.key, required this.title, required this.value});
+  final String title, value;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: Styles.style18,
+        ),
+        Text(
+          value,
+          style: Styles.styleBold18,
+        )
+      ],
     );
   }
 }

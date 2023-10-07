8e87d7a5607118e17b86df682cab9864111d5146
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-07 10:12:24 +0300

    create thank you view

---
+++ lib/Features/checkout/presentation/views/thank_you_view.dart
@@ -0,0 +1,13 @@
+import 'package:checkout_payment_ui/Features/checkout/presentation/views/widgets/thank_you_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ThankYouView extends StatelessWidget {
+  const ThankYouView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ThankYouViewBody(),
+    );
+  }
+}
---
+++ lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThankYouViewBody extends StatelessWidget {
+  const ThankYouViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

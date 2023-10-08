d606e27ccc866c8a74534267b5936bb0de1e41f4
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-08 10:11:56 +0300

    finish thank you card

--- lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
@@ -4,6 +4,7 @@ import 'package:checkout_payment_ui/Features/checkout/presentation/views/widgets
 import 'package:checkout_payment_ui/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ThankYouCard extends StatelessWidget {
   const ThankYouCard({
@@ -63,7 +64,39 @@ class ThankYouCard extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const CardInfoWidget()
+            const CardInfoWidget(),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  FontAwesomeIcons.barcode,
+                  size: 64,
+                ),
+                Container(
+                  width: 113,
+                  height: 58,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                          width: 1.50, color: Color(0xFF34A853)),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'PAID',
+                      textAlign: TextAlign.center,
+                      style: Styles.style24
+                          .copyWith(color: const Color(0xff34A853)),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: ((MediaQuery.sizeOf(context).height * .2 + 20) / 2) - 29,
+            ),
           ],
         ),
       ),

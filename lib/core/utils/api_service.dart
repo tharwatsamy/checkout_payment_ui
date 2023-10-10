2e262c4331e205981445c92466162cc0c77adcba
tharwatsamy <tharwatsamy2004@gmail.com>
2023-10-10 12:41:38 +0300

    create api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio = Dio();
+
+  Future<Response> post(
+      {required body,
+      required String url,
+      required String token,
+      String? contentType}) async {
+    var response = await dio.post(url,
+        data: body,
+        options: Options(
+          contentType: contentType,
+          headers: {'Authorization': "Bearer $token"},
+        ));
+
+    return response;
+  }
+}
--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,1 +1,9 @@
-class StripeService {}
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel paymentIntentInputModel)
+  // {
+
+  // }
+}
